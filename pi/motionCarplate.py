0bb686c82d5e8812e5622b790c7c62f2fe692d62
Vivian <43260480+ChanHueiMun@users.noreply.github.com>
2020-05-01 14:11:37 +0800

    Update motionCarplate.py

--- pi/motionCarplate.py
+++ pi/motionCarplate.py
@@ -4,6 +4,7 @@ from picamera import PiCamera
 from openalpr import Alpr
 import datetime
 import os
+import requests
 
 # Pin  Definitions
 pirSensorPin = 40
@@ -16,6 +17,11 @@ GPIO.setup(pirSensorPin, GPIO.IN)
 def scan(expectedCar):
     try:
         print("Starting scan")
+
+        # Send information over
+        carplateNum = ""
+        carplateList = []
+        endpoint = "http://192.168.1.94:5000/api/reservation/attend"
 
         # Take Photo
         print("Taking picture")
@@ -53,9 +59,20 @@ def scan(expectedCar):
                     print(candidate['plate'])
 
                     # See if it's a match w/ expected carplate
-                    if candidate['plate'] in expectedCar:
-                        print("FOUND")
-                        return
+                    #if candidate['plate'] in expectedCar:
+                        #print("FOUND")
+                        #return
+                    carplateNum = candidate(['plate'])
+                    #Can do some filtering here
+                    carplateList.append(carplateNum)
+        
+        print(carplateList)
+        carplateList = json.dumps(carplateList)
+        test = requests.post(url=endpoint, json=carplateList)
+        # Print out status code
+        print(test)
+        # Print out the information sent over
+        print(test.json())
     except:
         return
 
